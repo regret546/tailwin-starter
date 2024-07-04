581183348b61619126ddf76eb9a570031731c249
regret546 <iregret546@gmail.com>
2024-07-04 23:42:08 +0800

    add initial wavey animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
   },
   plugins: [require("./plugins/openVariant")],
 };
